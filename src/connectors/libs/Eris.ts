d2119f6be0e35ab7ad24655acbb9f1b34a5dccb8
Zero <62357050+Zer0625@users.noreply.github.com>
2026-06-26 18:59:14 -0500

    fix event

--- src/connectors/libs/Eris.ts
+++ src/connectors/libs/Eris.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any */
 import { Connector } from '../Connector';
 import { NodeOption } from '../../Shoukaku';
 
